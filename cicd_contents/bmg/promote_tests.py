4eb891f91fc328c839dc73d6700ae133c30cfb95
ashwini rathi <arathi@habana.ai>
2026-03-06 17:44:34 +0530

    [Internal/Common] way of Adding sglang kernel  ci cd test path (#49)
    Co-authored-by: Loveleen Amar <loveleen.amar@intel.com>

---
+++ cicd_contents/bmg/promote_tests.py
@@ -0,0 +1,3 @@
+PROMOTE_SGL_KERNEL_XPU_TESTS = [
+    "tests/test_activation.py::test_fused_gelu_mul[1-8-128]",
+]
---
+++ cicd_contents/cri/ci_tests.py
@@ -0,0 +1,3 @@
+CI_SGL_KERNEL_XPU_TESTS = [
+    "tests/test_activation.py::test_fused_silu_mul[1-1-128]",
+]
