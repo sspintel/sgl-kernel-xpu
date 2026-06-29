f1eb8c9e9c48889e93b142f22aab361eec4b9045
Manoj Kumar <manoj1.kumar@intel.com>
2026-06-29 04:31:22 +0000

    [Internal/CRI] inline definition of 'round_up_headdim'
    Signed-off-by: Manoj Kumar <manoj1.kumar@intel.com>

--- src/sycl/Utils.h
+++ src/sycl/Utils.h
@@ -336,7 +336,7 @@ int nextPowerOf2(uint32_t a) {
   if (a <= 1) return 1;
   return 1 << (32 - __builtin_clz(a - 1));
 };
-int round_up_headdim(int head_size) {
+inline int round_up_headdim(int head_size) {
   if (head_size <= 64) return 64;
   if (head_size <= 96) return 96;
   if (head_size <= 128) return 128;
